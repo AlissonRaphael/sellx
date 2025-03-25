18a9db2895e8abdaf654365493f6ebe014f25761
Alisson Oliveira <alissonraphaeloliveira@gmail.com>
2025-03-24 21:16:51 -0300

    chore: configure docs

---
+++ packages/docs/.storybook/preview.ts
@@ -0,0 +1,28 @@
+import type { Preview } from "@storybook/react";
+import { themes } from '@storybook/theming';
+
+const preview: Preview = {
+  parameters: {
+    controls: {
+      matchers: {
+        color: /(background|color)$/i,
+        date: /Date$/i,
+      },
+    },
+
+    docs: {
+      theme: themes.dark,
+    },
+
+    backgrounds: {
+      values: [
+        { name: 'Dark', value: '#333' },
+        { name: 'Light', value: '#F7F9F2' },
+      ],
+      default: 'Dark',
+    },
+  },
+
+};
+
+export default preview;
