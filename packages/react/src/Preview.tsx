e8b78379963da95c648a5f85d9c50eef5560cc68
Alisson Oliveira <alissonraphaeloliveira@gmail.com>
2025-04-05 20:46:32 -0300

    chore: config preview in react

---
+++ packages/react/src/Preview.tsx
@@ -0,0 +1,7 @@
+import { Badge } from "@/components/ui/badge"
+
+export default function Preview() {
+  return <div className="w-screen h-screen bg-slate-900">
+    <Badge variant="outline" className="text-amber-100">Test</Badge>
+  </div>
+}
--- packages/react/src/main.tsx
+++ packages/react/src/main.tsx
@@ -1,11 +1,11 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 
-import './global/style.css'
-import Demo from './Demo.tsx'
+import '@/global/style.css'
+import Preview from './Preview.tsx'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <Demo />
+    <Preview />
   </StrictMode>,
 )
