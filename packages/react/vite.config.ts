9efd1b5e8d46605957d3b0acba140f3a58ceaf46
Alisson Oliveira <alissonraphaeloliveira@gmail.com>
2025-04-05 20:44:08 -0300

    chore: config lib build in react

--- packages/react/vite.config.ts
+++ packages/react/vite.config.ts
@@ -1,6 +1,7 @@
-import path from 'path'
 import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import path from 'path'
+import react from '@vitejs/plugin-react-swc'
+import dts from 'vite-plugin-dts'
 import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
@@ -8,10 +9,28 @@ export default defineConfig({
   plugins: [
     react(),
     tailwindcss(),
+    dts({
+      entryRoot: path.resolve(__dirname, 'src'),
+      outDir: 'dist/types',
+      copyDtsFiles: true,
+      tsconfigPath: path.resolve(__dirname, 'tsconfig.app.json'),
+    }),
   ],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
     },
+  },
+  build: {
+    lib: {
+      entry: path.resolve(__dirname, 'src/index.ts'),
+      formats: ['es', 'cjs'],
+      fileName: (format: string) => `index.${format}.js`,
+      cssFileName: 'style',
+    },
+    rollupOptions: {
+      external: ['react', 'react-dom'],
+    },
+    outDir: 'dist',
   },
 })
